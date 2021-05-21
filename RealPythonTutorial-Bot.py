2fd6960cb56b5eeca0c2c65d9e6eea852fc9e659
Barry Weiss <guitar.zen2006@gmail.com>
2021-05-20 22:56:12 -0400

    configured Bot.py

---
+++ RealPythonTutorial-Bot.py
@@ -0,0 +1,28 @@
+# bot.py
+import os
+import discord
+from discord import guild
+from dotenv import load_dotenv
+
+
+# Download dotenv by pip install python-dotenv
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+my_guild = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    GUILD = discord.utils.get(client.guilds, name=my_guild)
+    print(
+        f'({client.user} is connected to the following guild:\n'
+        f'{GUILD.name}(id: {GUILD.id})'
+    )
+
+
+
+
+
+
+client.run(token)
